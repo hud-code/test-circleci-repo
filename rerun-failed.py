12cbe03116eb5a85a4c3fc5a2fade37e68fe109e
hud-code <zach.hudson@circleci.com>
2023-05-15 15:27:47 -0500

    testing new script

--- rerun-failed.py
+++ rerun-failed.py
@@ -43,33 +43,33 @@ for i in pipeline_dict['items']:
 
 print("{} has run {} times.".format(job_name, run_num))
 
-# # if job has run more than limit, don't retry
-# if run_num > retry_limit:
-#     print("Automatic rerun workflow limit has been reached. Not retrying again.")
-#     sys.exit(1)
-# else:
-#     for i in pipeline_dict['items']:
-#         if i['id'] == workflow_id:
+# if job has run more than limit, don't retry
+if run_num > retry_limit:
+    print("Automatic rerun workflow limit has been reached. Not retrying again.")
+    sys.exit(1)
+else:
+    for i in pipeline_dict['items']:
+        if i['id'] == workflow_id:
 
-#             payload = {
-#                 "from_failed": True,
-#                 "jobs": job_id
-#             }
+            payload = {
+                "from_failed": True,
+                "jobs": job_id
+            }
 
-#             headers = {
-#                 'content-type': "application/json",
-#                 'authorization': "Circle-Token: $CIRCLE_TOKEN"
-#             }
+            headers = {
+                'content-type': "application/json",
+                'authorization': "Circle-Token: $CIRCLE_TOKEN"
+            }
             
-#             conn.request("POST", "/api/v2/workflow/{}/rerun".format(workflow_id), payload, headers)
+            conn.request("POST", "/api/v2/workflow/{}/rerun".format(workflow_id), payload, headers)
             
-#             res = conn.getresponse()
-#             data = res.read()
+            res = conn.getresponse()
+            data = res.read()
             
-#             print(data.decode("utf-8"))
+            print(data.decode("utf-8"))
 
-#             print("Retrying job.")
+            print("Retrying job.")
             
-#             break
+            break
 
-#     sys.exit(0)
+    sys.exit(0)
