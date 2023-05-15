451fae077c5f4575235051adca8bbf14645aff5e
hud-code <zach.hudson@circleci.com>
2023-05-15 12:58:11 -0500

    testing rerun script

--- rerun-failed.py
+++ rerun-failed.py
@@ -6,7 +6,7 @@ conn = http.client.HTTPSConnection("circleci.com")
 
 headers = { 'authorization': "Circle-Token: $CIRCLE_TOKEN" }
 
-conn.request("GET", "/api/v2/pipeline/4d7c2311-8332-4c6e-97c9-fb92a93463d0/workflow", headers=headers)
+conn.request("GET", "/api/v2/pipeline/{<< pipeline.id >>}/workflow", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
